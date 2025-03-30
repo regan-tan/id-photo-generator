a7aad17d83fb6631d5b787c0e96d1eb4a38e5bba
Regan Tan <regantanyk@gmail.com>
2025-03-30 11:50:50 +0800

    still facing issues with integrating drive api

--- src/main/java/com/example/idphotogenerator/service/DownloadFile.java
+++ src/main/java/com/example/idphotogenerator/service/DownloadFile.java
@@ -1,4 +1,9 @@
 package com.example.idphotogenerator.service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.javanet.NetHttpTransport;
@@ -7,30 +12,26 @@ import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Arrays;
 
 public class DownloadFile {
     public static ByteArrayOutputStream downloadFile(String fileId) throws IOException {
         // Load credentials
         GoogleCredentials credentials = GoogleCredentials.getApplicationDefault()
-            .createScoped(Arrays.asList(DriveScopes.DRIVE_FILE));
+                .createScoped(Arrays.asList(DriveScopes.DRIVE_FILE));
         HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(credentials);
-        
+
         // Build Drive service
-        Drive service = new Drive.Builder(new NetHttpTransport(), 
-                                         GsonFactory.getDefaultInstance(), 
-                                         requestInitializer)
-            .setApplicationName("Drive Sample")
-            .build();
-        
+        Drive service = new Drive.Builder(new NetHttpTransport(),
+                GsonFactory.getDefaultInstance(),
+                requestInitializer)
+                .setApplicationName("Drive Sample")
+                .build();
+
         try {
-            OutputStream outputStream = new ByteArrayOutputStream();
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream(); // ✅ Correct usage
             service.files().get(fileId)
-                .executeMediaAndDownloadTo(outputStream);
-            return (ByteArrayOutputStream) outputStream;
+                    .executeMediaAndDownloadTo(outputStream);
+            return outputStream;
         } catch (GoogleJsonResponseException e) {
             System.err.println("Unable to download file: " + e.getDetails());
             throw e;
--- src/main/java/com/example/idphotogenerator/service/UploadImage.java
+++ src/main/java/com/example/idphotogenerator/service/UploadImage.java
@@ -1,5 +1,10 @@
 package com.example.idphotogenerator.service;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.FileContent;
 import com.google.api.client.http.HttpRequestInitializer;
@@ -8,39 +13,38 @@ import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
-import java.io.IOException;
-import java.util.Arrays;
+
 
 public class UploadImage {
-    public static String uploadImage(String imagePath, String imageName) throws IOException {
-        // Load credentials
+
+    private static Drive getDriveService() throws IOException {
         GoogleCredentials credentials = GoogleCredentials.getApplicationDefault()
-            .createScoped(Arrays.asList(DriveScopes.DRIVE_FILE));
+                .createScoped(Arrays.asList(DriveScopes.DRIVE));
         HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(credentials);
-        
-        // Build Drive service
-        Drive service = new Drive.Builder(new NetHttpTransport(), 
-                                         GsonFactory.getDefaultInstance(), 
-                                         requestInitializer)
-            .setApplicationName("Drive Sample")
-            .build();
-        
-        // Prepare file metadata
+
+        return new Drive.Builder(new NetHttpTransport(),
+                GsonFactory.getDefaultInstance(),
+                requestInitializer)
+                .setApplicationName("Drive Sample")
+                .build();
+    }
+
+    public static String uploadImage(String imagePath, String imageName) throws IOException {
+        Drive service = getDriveService();
+
         File fileMetadata = new File();
         fileMetadata.setName(imageName);
-        
-        // Prepare file content
+
         java.io.File filePath = new java.io.File(imagePath);
         FileContent mediaContent = new FileContent("image/jpeg", filePath);
-        
+
         try {
-            // Upload file
             File file = service.files().create(fileMetadata, mediaContent)
-                .setFields("id")
-                .execute();
-            
+                    .setFields("id")
+                    .execute();
             System.out.println("File ID: " + file.getId());
             return file.getId();
         } catch (GoogleJsonResponseException e) {
@@ -48,5 +52,28 @@ public class UploadImage {
             throw e;
         }
     }
-}
 
+    // List image files from Google Drive
+    public static List<File> listImageFiles() throws IOException {
+        Drive service = getDriveService();
+        FileList result = service.files().list()
+                .setQ("mimeType contains 'image/'")
+                .setFields("files(id, name)")
+                .execute();
+    
+        return result.getFiles(); // return full File objects
+    }
+    
+
+    // Download specific image by fileId
+    public static String downloadImageById(String fileId, String outputPath) throws IOException {
+        Drive service = getDriveService();
+
+        java.io.File outputFile = new java.io.File(outputPath);
+        try (FileOutputStream outputStream = new FileOutputStream(outputFile)) {
+            service.files().get(fileId).executeMediaAndDownloadTo(outputStream);
+        }
+
+        return outputPath;
+    }
+}
